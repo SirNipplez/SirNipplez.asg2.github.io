5d7249cb72141a184168259cdf597d731e958dbc
SirNipplez <79791831+SirNipplez@users.noreply.github.com>
2023-11-29 17:58:55 -0700

    Update assign2.js

--- js/assign2.js
+++ js/assign2.js
@@ -1,11 +1,5 @@
 
 
-
-/* url of song api --- https versions hopefully a little later this semester */	
-const api = 'https://www.randyconnolly.com/funwebdev/3rd/api/music/songs-nested.php';
-
- 
-
 /* note: you may get a CORS error if you try fetching this locally (i.e., directly from a
    local file). To work correctly, this needs to be tested on a local web server.  
    Some possibilities: if using Visual Code, use Live Server extension; if Brackets,
@@ -16,7 +10,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
    setupMain();
 
    //from lecture
-   const url = `http://www.randyconnolly.com/funwebdev/3rd/api/music/songs-nested.php`;
+   const url = `https://www.randyconnolly.com/funwebdev/3rd/api/music/songs-nested.php`;
    
    let songs = localStorage.getItem("songs");
    if (!songs) {
